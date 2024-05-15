c32fad9ad02ead233af387af1a6637678edc54d6
tuoiiii332 <brianchubypun0suy@hotmail.com>
2024-05-16 06:59:37 +0700

    aptos

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function lengthOfLIS(nums) {
-  if (nums.length === 0) return 0;
-  const dp = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-        max = Math.max(max, dp[i]);
-      }
-    }
-  }
-  return max;
+function largestNumber(nums) {
+  if (nums.every((num) => num === 0)) return "0";
+  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
 }
